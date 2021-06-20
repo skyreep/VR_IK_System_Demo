337d05fb18becda2a604fb9528b371946f2e09f6
Skyreep <skyreep@gmail.com>
2021-06-19 20:39:46 -0400

    Add files via upload

---
+++ Scripts/Testing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(20, 10);
+    }
+}
